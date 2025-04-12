515390fd94f96a9687d2d9452e204c900ebfdef9
FirstFlush <firstflush@protonmail.com>
2025-04-12 13:29:29 -0700

    updated vite config preview and allowedHosts properties

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,5 +3,18 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [tailwindcss(), sveltekit()]
+	plugins: [
+		tailwindcss(), 
+		sveltekit()
+	],
+	preview: {
+		host: '0.0.0.0',
+		port: 4173,
+		allowedHosts: [
+			'useragent.one',
+			'www.useragent.one',
+			'localhost',
+			'127.0.0.1',
+		],
+	}
 });
