dff673ba914589ac0848e3aab8cacd12feb6644c
FirstFlush <firstflush@protonmail.com>
2025-04-13 10:48:59 -0700

    troubleshooting node cron

--- backend/src/plugins/scheduler.ts
+++ backend/src/plugins/scheduler.ts
@@ -2,8 +2,10 @@ import cron from 'node-cron';
 import { refreshUserAgentData } from '../tasks';
 import logger from '../utils/logger';
 
+logger.info("Scheduler file imported and cron is registered.");
+
 // UTC time
-cron.schedule("27 17 * * *", async () => {
+cron.schedule("55 17 * * *", async () => {
     if (process.env.NODE_ENV === "production") {
         logger.info("Running scheduled UA refresh task");
         await refreshUserAgentData();
