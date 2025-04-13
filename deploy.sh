5b8857126b3dac9987a830eab74e8185fdf5fc3a
FirstFlush <firstflush@protonmail.com>
2025-04-13 09:20:31 -0700

    added --no-pull option to deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,18 +1,38 @@
 #!/bin/bash
-
-# Run as sudo!
-
-# Deploy script for useragent-one.
+#
+# Deploy script for useragent-one:
+#
 # - pull updates from github
 # - rebuild docker containers with new updates
 # - reload nginx
+# - run with "--no-pull" to skip pulling from rpeo
+#
+# Usage:
+#
+# sudo chmod +x deploy.sh
+# - run as sudo!
+#
+# Examples:
+#
+# sudo ./deploy.sh
+# sudo ./deploy.sh --no-pull
 
+ 
 set -e  # Exit on error
+
+SKIP_PULL=false
+if [[ "$1" == "--no-pull" ]]; then
+  SKIP_PULL=true
+fi
 
 echo "Updating app on server..."
 
-echo -e "\n[1/6] Pulling latest changes..."
-git pull origin master
+if [ "$SKIP_PULL" = false ]; then
+  echo -e "\n[1/6] Pulling latest changes..."
+  git pull origin master
+else
+  echo -e "\n[1/6] Skipping git pull (per --no-pull)"
+fi
 
 echo -e "\n[2/6] Shutting down containers..."
 docker compose down
